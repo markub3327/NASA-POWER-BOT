e0a18abff004599115a4c529f54596107e678ed7
Martin Kubovcik <markub3327@gmail.com>
2022-05-17 16:07:56 +0200

    + Point - hourly

--- bot.py
+++ bot.py
@@ -62,10 +62,11 @@ F = int(
     args.area_width * 2 * args.area_height * 2
 )  # NASA POWER resolution is 1/2 deg and 1/2 deg !!!
 X_all = np.zeros((T, P, F), dtype=np.float32)
-y_all = np.zeros((T, P, 1), dtype=np.float32)
+y_hourly_all = np.zeros(((T * 24), P, 1), dtype=np.float32)
+y_daily_all = np.zeros((T, P, 1), dtype=np.float32)
 
 # Timesteps
-t = 0
+t_daily, t_hourly = 0, 0
 for year in range(args.year_start, args.year_end + 1):
 
     # Patches
@@ -84,7 +85,7 @@ for year in range(args.year_start, args.year_end + 1):
         print(f"Area: {latitude_min}, {latitude_max}, {longitude_min}, {longitude_max}")
         print("----------------------------------------------------------\n")
 
-        # Region
+        # Region - daily
         response_region = requests.get(
             f"https://power.larc.nasa.gov/api/temporal/daily/regional?start={year}0101&end={year}1231&latitude-min={latitude_min}&latitude-max={latitude_max}&longitude-min={longitude_min}&longitude-max={longitude_max}&community=re&parameters=ALLSKY_SFC_SW_DWN&format=json&header=true&time-standard=utc",
             verify=True,
@@ -107,13 +108,13 @@ for year in range(args.year_start, args.year_end + 1):
                     "ALLSKY_SFC_SW_DWN"
                 ]
                 x = list(features_region.values())
-                X_all[t : t + len(x), p, f] = x
+                X_all[t_daily : t_daily + len(x), p, f] = x
         else:
             raise ValueError(
                 f"Cannot download region dataset with status code {response_region.status_code} 😟"
             )
 
-        # Point
+        # Point - daily
         response_target = requests.get(
             f"https://power.larc.nasa.gov/api/temporal/daily/point?start={year}0101&end={year}1231&latitude={target_locations[key][0]}&longitude={target_locations[key][1]}&community=re&parameters=ALLSKY_SFC_SW_DWN&format=json&header=true&time-standard=utc",
             verify=True,
@@ -133,22 +134,57 @@ for year in range(args.year_start, args.year_end + 1):
 
             features_point = content["properties"]["parameter"]["ALLSKY_SFC_SW_DWN"]
             y = list(features_point.values())
-            y_all[t : t + len(y), p, 0] = y
+            y_daily_all[t_daily : t_daily + len(y), p, 0] = y
         else:
             raise ValueError(
                 f"Cannot download point dataset with status code {response_target.status_code} 😟\n"
             )
 
+        # Point - hourly
+        if year >= 2001:
+            response_target = requests.get(
+                f"https://power.larc.nasa.gov/api/temporal/hourly/point?start={year}0101&end={year}1231&latitude={target_locations[key][0]}&longitude={target_locations[key][1]}&community=re&parameters=ALLSKY_SFC_SW_DWN&format=json&header=true&time-standard=utc",
+                verify=True,
+                timeout=args.timeout,
+            )
+            if response_target.status_code == 200:
+                content = response_target.json()
+
+                # Header
+                units = content["parameters"]["ALLSKY_SFC_SW_DWN"]["units"]
+                name = content["parameters"]["ALLSKY_SFC_SW_DWN"]["longname"]
+                fill_value = content["header"][
+                    "fill_value"
+                ]  # represents missing values (measurement error)
+                print(f"{name} ({units})")
+                print(f"Fill value: {fill_value}", "\n")
+
+                features_point = content["properties"]["parameter"]["ALLSKY_SFC_SW_DWN"]
+                y = list(features_point.values())
+                y_hourly_all[t_hourly : t_hourly + len(y), p, 0] = y
+            else:
+                raise ValueError(
+                    f"Cannot download point dataset with status code {response_target.status_code} 😟\n"
+                )
+        else:
+            y_hourly_all[t_hourly : t_hourly + (days_per_year * 24), p, 0] = -1
+            print(f"Year {year} is too early for hourly data. Filled with missing value -1.\n")
+
     print("Dataset downloaded 🙂\n")
 
-    t += days_per_year
+    t_daily += days_per_year
+    t_hourly += (days_per_year * 24)
+
+print(t_daily, " ", t_hourly)
 
 # replace bad values with fill value -1 !!!
 X_all[X_all < 0] = -1
-y_all[y_all < 0] = -1
+y_daily_all[y_daily_all < 0] = -1
+y_hourly_all[y_hourly_all < 0] = -1
 
 print(f"Inputs shape: {X_all.shape}")
-print(f"Target shape: {y_all.shape}")
+print(f"Target daily shape: {y_daily_all.shape}")
+print(f"Target hourly shape: {y_hourly_all.shape}")
 
 # Descriptive Statistics
 print(f"Minimum: {np.min(X_all)}")
@@ -157,20 +193,30 @@ print(f"Mean: {np.mean(X_all)}")
 print(f"Standard deviation: {np.std(X_all)}\n")
 
 # Descriptive Statistics
-print(f"Minimum: {np.min(y_all)}")
-print(f"Maximum: {np.max(y_all)}")
-print(f"Mean: {np.mean(y_all)}")
-print(f"Standard deviation: {np.std(y_all)}\n")
+print(f"Minimum: {np.min(y_daily_all)}")
+print(f"Maximum: {np.max(y_daily_all)}")
+print(f"Mean: {np.mean(y_daily_all)}")
+print(f"Standard deviation: {np.std(y_daily_all)}\n")
+
+# Descriptive Statistics
+print(f"Minimum: {np.min(y_hourly_all)}")
+print(f"Maximum: {np.max(y_hourly_all)}")
+print(f"Mean: {np.mean(y_hourly_all)}")
+print(f"Standard deviation: {np.std(y_hourly_all)}\n")
 
 # save dataset
-np.savez_compressed("dataset", X=X_all, y=y_all)
+np.savez_compressed("dataset", X=X_all, y_daily=y_daily_all, y_hourly=y_hourly_all)
 
 # inputs distribution
 sns.displot(X_all.reshape((-1, X_all.shape[-1])), kde=True)
 plt.title("Region")
 
 # target distribution
-sns.displot(y_all.reshape((-1, y_all.shape[-1])), kde=True)
-plt.title("Point")
+sns.displot(y_daily_all.reshape((-1, y_daily_all.shape[-1])), kde=True)
+plt.title("Point - daily")
+
+# target distribution
+sns.displot(y_hourly_all.reshape((-1, y_hourly_all.shape[-1])), kde=True)
+plt.title("Point - hourly")
 
 plt.show()
